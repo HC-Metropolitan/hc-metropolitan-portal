953c9ea1a2c07269e7b7081208d86eb9b9a4aa1a
peco2282 <87461070+peco2282@users.noreply.github.com>
2025-04-13 12:58:08 +0900

    fix imports

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,6 @@
   </footer>
 </template>
 <script setup lang="ts">
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
