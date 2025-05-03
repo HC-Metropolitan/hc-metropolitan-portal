703bc9b9446b67283df00237633244d8c2bed56d
peco2282 <87461070+peco2282@users.noreply.github.com>
2025-05-03 18:00:18 +0900

    Add Serverdata

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,9 +1,13 @@
 <template>
+  <ServerInfo/>
   <Gallery/>
+  <ServerDescription/>
 </template>
 
 <script setup lang="ts">
 import Gallery from "../components/Gallery.vue";
+import ServerInfo from "../components/ServerInfo.vue";
+import ServerDescription from "../components/ServerDescription.vue";
 </script>
 
 <style scoped>
